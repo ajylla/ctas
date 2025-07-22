6461f6b7950bd7667d42ea13867706ab7d77c40e
ajylla <aleksi.ylilaurila@gmail.com>
2025-07-22 19:53:57 +0300

    Added Stamp dataclass

--- ctas.py
+++ ctas.py
@@ -1,5 +1,17 @@
 import sqlite3
 from pathlib import Path
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class Stamp:
+    time: datetime
+    stamper: str
+    sid: int
+    sname: str
+    stype: str
+    status: str
 
 
 db_name = "db.db"
@@ -8,7 +20,6 @@ p = Path('.')
 db_con = sqlite3.connect("db.db")
 db_cur = db_con.cursor()
 if db_name not in p:
-    db_cur.execute("CREATE TABLE stamp(time, stamper, sid, sname, type, status)")
-
+    db_cur.execute("CREATE TABLE stamp(time, stamper, sid, sname, stype, status)")
 
 db_con.close()
