8f1a29e27add3b49f2f73979432b1b1e8fe2f458
ajylla <aleksi.ylilaurila@gmail.com>
2025-07-23 12:23:18 +0300

    Moved database initialization code to db_init() Added data directory, and stamps.json file

--- ctas.py
+++ ctas.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 from pathlib import Path
 from dataclasses import dataclass
 from datetime import datetime
@@ -27,14 +28,20 @@ def write_stamp(stamp, db_cur) -> None:
     db_cur.execute("INSERT INTO stamp(?, ?, ?, ?, ?, ?)", data)
 
 
+def db_init():
+    db_name = "db.db"
+    p = Path(f"./{db_name}")
+
+    db_exists = p.exists()
+    db_con = sqlite3.connect(db_name)
+    db_cur = db_con.cursor()
+
+    if not db_exists:
+        db_cur.execute("CREATE TABLE stamp(time, stamper, sid, sname, stype, status)")
+
+    return db_con, db_cur
 
 
-db_name = "db.db"
-p = Path(f"./{db_name}")
-
-db_con = sqlite3.connect(db_name)
-db_cur = db_con.cursor()
-if not p.exists():
-    db_cur.execute("CREATE TABLE stamp(time, stamper, sid, sname, stype, status)")
-
-db_con.close()
+if __name__ == "__main__":
+    db_con, db_cur = db_init()
+    db_con.close()
